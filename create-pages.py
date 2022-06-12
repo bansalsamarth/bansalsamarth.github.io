0cbd16ca953fa4e4e73f351a8483c3d727780cb2
Samarth Bansal <samarthbansal42@gmail.com>
2022-06-12 13:21:16 +0530

    script updated

--- create-pages.py
+++ create-pages.py
@@ -15,3 +15,11 @@ for file_name in migration_file_list:
     os.system("pandoc SamarthBlog/migrations/" + file_name + " -o ./" + file_name[:-3] + ".html --template templates/post-template.html")
     print("pandoc SamarthBlog/migrations/" + file_name + " -o ./" + file_name[:-3] + ".html --template templates/post-template.html")
     print(file_name, "...published", "\n\n")
+
+published_file_list = os.listdir("SamarthBlog/published-articles/")
+print(len(published_file_list))
+
+for file_name in published_file_list:
+    os.system("pandoc SamarthBlog/published-articles/" + file_name + " -o published/" + file_name[:-3] + ".html --template templates/published-article-template.html")
+    print("pandoc SamarthBlog/published-articles/" + file_name + " -o published/" + file_name[:-3] + ".html --template templates/published-article-template.html")
+    print(file_name, "...published", "\n\n")
